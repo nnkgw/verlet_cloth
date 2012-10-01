b792a7857958aeff5e4ee369ed92fb48dc7ecdf7
nnkgw <nkgw@s241.xrea.com>
2012-10-01 23:40:04 +0900

    add cloth

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ public:
     }
   }
 
-  glm::vec3& GetPostion() { return m_Positin;   }
+  glm::vec3& GetPostion() { return m_Positin; }
   void       AddPosition(const glm::vec3 pos){
     if (m_IsMovable){
       m_Positin += pos;
@@ -80,6 +80,12 @@ private:
   CParticle* GetParticle(int w, int h) {return &m_Particles[ h * m_Width + w ];}
   void       MakeConstraint(CParticle* p1, CParticle* p2){ m_Constraints.push_back(CConstraint(p1,p2));}
 
+  void DrawTriangle(CParticle* p1, CParticle* p2, CParticle* p3, const glm::vec3 color){
+    glColor3fv((GLfloat*)&color);
+    glVertex3fv((GLfloat*)&(p1->GetPostion()));
+    glVertex3fv((GLfloat*)&(p2->GetPostion()));
+    glVertex3fv((GLfloat*)&(p3->GetPostion()));
+  }
 public:
   CCloth(float width, float height, int num_width, int num_height):
   m_Width(num_height),
@@ -87,12 +93,15 @@ public:
     m_Particles.resize(m_Width * m_Height);
     for(int w = 0; w < m_Width; w++){
       for(int h = 0; h < m_Height; h++){
-        glm::vec3 pos( width  * ( w / (float)m_Width  ),
-                      -height * ( h / (float)m_Height ),
+        glm::vec3 pos( width  * ((float)w/(float)m_Width ) - width  * 0.5f,
+                      -height * ((float)h/(float)m_Height) + height * 0.5f,
                        0.0f );
         bool is_movable = (h == 0) ? false : true;
         glm::vec3 gravity( 0.0f, -9.8f, 0.0f );
         m_Particles[ h * m_Width + w ] = CParticle(is_movable, pos, gravity);
+#if 1
+        printf("pos(%f,%f,%f)\n",pos.x, pos.y, pos.z);
+#endif
       }
     }
     for(int w = 0; w < m_Width; w++){
@@ -107,6 +116,29 @@ public:
     }
   }
   ~CCloth(){}
+
+  void Render(){
+    glBegin(GL_TRIANGLES);
+    for(int w = 0; w < m_Width - 1; w++){
+      for(int h = 0; h < m_Height - 1; h++){
+        glm::vec3 col(1.0f, 0.0f, 0.0f);
+        if ( w % 2 ){ col = glm::vec3(0.0f, 1.0f, 0.0f);}
+        DrawTriangle(GetParticle(w+1,h  ), GetParticle(w,  h), GetParticle(w, h+1), col);
+        DrawTriangle(GetParticle(w+1,h+1), GetParticle(w+1,h), GetParticle(w, h+1), col);
+      }
+    }
+    glEnd();
+  }
+  void Update(float dt){
+    std::vector<CParticle>::iterator particle;
+    for(particle = m_Particles.begin(); particle != m_Particles.end(); particle++){
+      (*particle).Update(dt);
+    }
+    std::vector<CConstraint>::iterator constraint;
+    for(constraint = m_Constraints.begin(); constraint != m_Constraints.end(); constraint++){
+      (*constraint).Satisfy();
+    }
+  }
 };
 
 struct sApplication{
@@ -114,6 +146,7 @@ struct sApplication{
 };
 
 sApplication g_Application;
+CCloth g_Cloth(2.0f, 2.0f, 50, 50);
 
 void init(void){
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -123,7 +156,7 @@ void init(void){
 }
 
 void update_physics(GLfloat dt){
-
+  g_Cloth.Update(dt);
 }
 
 void display(void){
@@ -132,6 +165,8 @@ void display(void){
   glPushMatrix();
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS); 
+
+#if 0
     GLfloat red[] = { 0.8, 0.2, 0.2, 1.0 };
     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, red);
     //glEnable(GL_COLOR_MATERIAL);
@@ -144,6 +179,11 @@ void display(void){
     glFrontFace(GL_CW);
     glutSolidTeapot(1.0f);
     glFrontFace(GL_CCW);
+#else
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_NORMALIZE);
+    g_Cloth.Render();
+#endif
 
   glPopMatrix();
 
@@ -170,7 +210,7 @@ void reshape(int width, int height){
   glLoadIdentity();
     
   // pos, tgt, up
-  gluLookAt(0.0f, 2.5f, 5.5f, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  gluLookAt(0.0f, 00.0f, 5.0f, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
   glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
   glLightfv(GL_LIGHT0, GL_DIFFUSE,  lightDiffuse);
