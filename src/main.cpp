25fd42f9dbdaa8e3d1d0f603857e35aaa17ccec6
nnkgw <nkgw@s241.xrea.com>
2014-08-29 15:16:55 +0900

    refactoring only

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ private:
   glm::vec3 m_Acceleration;
 
 public:
-  CParticle(bool is_movable, glm::vec3 position, glm::vec3 acceleration) :
+  CParticle(bool is_movable, glm::vec3& position, glm::vec3& acceleration) :
   m_IsMovable(is_movable),
   m_Position(position),
   m_OldPosition(position),
@@ -43,16 +43,6 @@ public:
   void       AddPosition(const glm::vec3 pos){
     if (m_IsMovable){
       m_Position += pos;
-    }
-  }
-  void       AddForce(const glm::vec3 force){
-    if (m_IsMovable){
-      m_Acceleration += force;
-    }
-  }
-  void       SetForce(const glm::vec3 force){
-    if (m_IsMovable){
-      m_Acceleration = force;
     }
   }
 };
@@ -81,37 +71,6 @@ public:
   }
 };
 
-class CSpring{
-private:
-  float      m_Distance;
-  CParticle* m_Particle1;
-  CParticle* m_Particle2;
-  float      m_Stiffness;
-public:
-  CSpring(CParticle* p1, CParticle* p2, float stiffness) :
-  m_Distance(0.0f),
-  m_Particle1(p1),
-  m_Particle2(p2),
-  m_Stiffness(stiffness)
-  {
-    //glm::vec3 p1_to_p2 = m_Particle2->GetPosition() - m_Particle1->GetPosition();
-    //m_Distance = glm::length(p1_to_p2);
-  }
-  
-  void ApplyForce(){
-    glm::vec3 p1_to_p2 = m_Particle2->GetPosition() - m_Particle1->GetPosition();
-    float     diff     = glm::length(p1_to_p2) - m_Distance;
-    glm::vec3 force    = m_Stiffness * glm::normalize(p1_to_p2) * diff; 
-    m_Particle1->SetForce( force);
-    m_Particle2->SetForce(-force);
-  }
-  void ResetForce(){
-    glm::vec3 gravity( 0.0f, -0.98f, 0.0f );
-    m_Particle1->SetForce(gravity);
-    m_Particle2->SetForce(gravity);
-  }
-};
-
 class CBall{
 private:
   float     m_Frequency;
@@ -129,6 +88,7 @@ public:
     m_Frequency += dt / 5.0f;
     if (m_Frequency > 3.14f * 2.0f){ m_Frequency -= 3.14 * 2.0f; }
   }
+
   void Render(){
     glTranslatef(m_Position.x, m_Position.y, m_Position.z);
     static const glm::vec3 color(0.0f, 0.0f, 1.0f);
@@ -145,14 +105,10 @@ private:
   int                      m_Width;
   int                      m_Height;
   std::vector<CParticle>   m_Particles;
-  std::vector<CParticle>   m_GoalParticles;
   std::vector<CConstraint> m_Constraints;
-  std::vector<CSpring>     m_Springs;
   
   CParticle* GetParticle(int w, int h) {return &m_Particles[ h * m_Width + w ];}
-  CParticle* GetGoalParticle(int w, int h) {return &m_GoalParticles[ h * m_Width + w ];}
-  void       MakeConstraint(CParticle* p1, CParticle* p2) { m_Constraints.push_back(CConstraint(p1,p2));}
-  void       MakeSpring(CParticle* p1, CParticle* p2, float stiffness) { m_Springs.push_back(CSpring(p1,p2, stiffness)); }
+  void       MakeConstraint(CParticle* p1, CParticle* p2) { m_Constraints.push_back(CConstraint(p1, p2));}
 
   void DrawTriangle(CParticle* p1, CParticle* p2, CParticle* p3, const glm::vec3 color){
     glColor3fv((GLfloat*)&color);
@@ -166,7 +122,6 @@ public:
   m_Width(num_height),
   m_Height(num_height) {
     m_Particles.resize(m_Width * m_Height);
-    m_GoalParticles.resize(m_Width * m_Height);
     for(int w = 0; w < m_Width; w++){
       for(int h = 0; h < m_Height; h++){
         glm::vec3 pos( width  * ((float)w/(float)m_Width ) - width  * 0.5f,
@@ -175,30 +130,25 @@ public:
         bool is_movable = (h == 0) ? false : true;
         glm::vec3 gravity( 0.0f, -0.98f, 0.0f );
         m_Particles[ h * m_Width + w ] = CParticle(is_movable, pos, gravity);
-        glm::vec3 goal_pos( width  * ((float)w/(float)m_Width ) - width  * 0.5f,
-                           -height * ((float)h/(float)m_Height) + height * 0.5f -1.0f,
-                            0.0f );
-        m_GoalParticles[ h * m_Width + w ] = CParticle(false, goal_pos, gravity);
-        MakeSpring(GetParticle(w, h), GetGoalParticle(w, h), 10.0f);
       }
     }
     for(int w = 0; w < m_Width; w++){
       for(int h = 0; h < m_Height; h++){           // structual constraint
-        if (w < m_Width  - 1){ MakeConstraint(GetParticle(w,h), GetParticle(w+1,h  )); }
-        if (h < m_Height - 1){ MakeConstraint(GetParticle(w,h), GetParticle(w,  h+1)); }
+        if (w < m_Width  - 1){ MakeConstraint(GetParticle(w, h), GetParticle(w+1, h  )); }
+        if (h < m_Height - 1){ MakeConstraint(GetParticle(w, h), GetParticle(w,   h+1)); }
         if (w < m_Width  - 1 && h < m_Height - 1){ // shear constraint
-          MakeConstraint(GetParticle(w,  h), GetParticle(w+1,h+1));
-          MakeConstraint(GetParticle(w+1,h), GetParticle(w,  h+1));
+          MakeConstraint(GetParticle(w,   h), GetParticle(w+1, h+1));
+          MakeConstraint(GetParticle(w+1, h), GetParticle(w,   h+1));
         }
       }
     }
     for(int w = 0; w < m_Width; w++){
       for(int h = 0; h < m_Height; h++){           // bend constraint
-        if (w < m_Width  - 2){ MakeConstraint(GetParticle(w,h), GetParticle(w+2,h  )); }
-        if (h < m_Height - 2){ MakeConstraint(GetParticle(w,h), GetParticle(w,  h+2)); }
+        if (w < m_Width  - 2){ MakeConstraint(GetParticle(w, h), GetParticle(w+2, h  )); }
+        if (h < m_Height - 2){ MakeConstraint(GetParticle(w, h), GetParticle(w,   h+2)); }
         if (w < m_Width  - 2 && h < m_Height - 2){
-          MakeConstraint(GetParticle(w,  h), GetParticle(w+2,h+2));
-          MakeConstraint(GetParticle(w+2,h), GetParticle(w,  h+2));
+          MakeConstraint(GetParticle(w,   h), GetParticle(w+2, h+2));
+          MakeConstraint(GetParticle(w+2, h), GetParticle(w,   h+2));
         }
       }
     }
@@ -212,39 +162,23 @@ public:
       for(int h = 0; h < m_Height - 1; h++){
         glm::vec3 col(1.0f, 0.6f, 0.6f);
         if ( col_idx++ % 2 ){ col = glm::vec3(1.0f, 1.0f, 1.0f);}
-        DrawTriangle(GetParticle(w+1,h  ), GetParticle(w,  h), GetParticle(w, h+1), col);
-        DrawTriangle(GetParticle(w+1,h+1), GetParticle(w+1,h), GetParticle(w, h+1), col);
-/*
-        glm::vec3 goal_col(1.0f, 0.0f, 0.0f);
-        if ( col_idx++ % 2 ){ goal_col = glm::vec3(0.0f, 0.0f, 1.0f);}
-        DrawTriangle(GetGoalParticle(w+1,h  ), GetGoalParticle(w,  h), GetGoalParticle(w, h+1), goal_col);
-        DrawTriangle(GetGoalParticle(w+1,h+1), GetGoalParticle(w+1,h), GetGoalParticle(w, h+1), goal_col);
-*/
+        DrawTriangle(GetParticle(w+1,h  ), GetParticle(w,   h), GetParticle(w, h+1), col);
+        DrawTriangle(GetParticle(w+1,h+1), GetParticle(w+1, h), GetParticle(w, h+1), col);
       }
     }
     glEnd();
   }
+
   void Update(float dt, CBall* ball, int iteraion){
     std::vector<CParticle>::iterator particle;
     for(particle = m_Particles.begin(); particle != m_Particles.end(); particle++){
       (*particle).Update(dt);
     }
- /*
-    float time = (float)glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
-    std::vector<CSpring>::iterator spring;
-    for(spring = m_Springs.begin(); spring != m_Springs.end(); spring++){
-      if (time < 2.0f){
-        (*spring).ApplyForce();
-      }else{
-        (*spring).ResetForce();
-      }
-    }
-*/
     for(int i = 0; i < iteraion; i++){
       for(particle = m_Particles.begin(); particle != m_Particles.end(); particle++){
         glm::vec3 vec    = (*particle).GetPosition() - ball->GetPosition();
         float     length = glm::length(vec);
-        float     radius = ball->GetRadius() * 1.4f;
+        float     radius = ball->GetRadius() * 1.4f; // fake radius
         if (length < radius) {
           (*particle).AddPosition(glm::normalize(vec) * (radius - length));
         }
@@ -278,19 +212,18 @@ void render_string(std::string& str, int w, int h, int x0, int y0) {
   glDisable(GL_LIGHTING);
   glMatrixMode(GL_PROJECTION);
   glPushMatrix();
-    glLoadIdentity();
-    gluOrtho2D(0, w, h, 0);
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-      glLoadIdentity();
-      glRasterPos2f(x0, y0);
-      int size = (int)str.size();
-      for(int i = 0; i < size; ++i){
-        char ic = str[i];
-        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ic);
-      }
-    glPopMatrix();
-    glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0, w, h, 0);
+  glMatrixMode(GL_MODELVIEW);
+  glPushMatrix();
+  glLoadIdentity();
+  glRasterPos2f(x0, y0);
+  int size = (int)str.size();
+  for(int i = 0; i < size; ++i){
+    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, str[i]);
+  }
+  glPopMatrix();
+  glMatrixMode(GL_PROJECTION);
   glPopMatrix();
   glMatrixMode(GL_MODELVIEW);
 }
@@ -347,16 +280,13 @@ void reshape(int width, int height){
   gluPerspective(30.0, (double)width / (double)height, 0.0001f, 1000.0f);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-    
-  // pos, tgt, up
-  gluLookAt(0.0f, 00.0f, 5.0f, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+  gluLookAt(0.0f, 00.0f, 5.0f, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // pos, tgt, up
 
   glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
   glLightfv(GL_LIGHT0, GL_DIFFUSE,  lightDiffuse);
   glLightfv(GL_LIGHT0, GL_AMBIENT,  lightAmbient);
   glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecular);
-
-  //glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
 }
 
 void idle(void){
@@ -376,7 +306,6 @@ void special(int key, int x, int y){
   if (key == GLUT_KEY_UP) {
     g_Application.m_IterationNum++;
   }
-
   if (key == GLUT_KEY_DOWN) {
     if (g_Application.m_IterationNum > 1){
       g_Application.m_IterationNum--;
